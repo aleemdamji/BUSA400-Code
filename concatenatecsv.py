75ece3d6d6783b5d048c771284ac2d1cc0e56071
aleemdamji <35583239+aleemdamji@users.noreply.github.com>
2019-10-27 21:59:31 -0400

    Add files via upload
    Random code snippet used to concatenate all CSVs in a folder

---
+++ concatenatecsv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 27 21:40:39 2019
+
+@author: home
+"""
+
+
+
+import os
+import glob
+import pandas as pd
+
+#User Inputs
+newwd = "/Users/home/Dropbox/Social Media and Humanitarian Operations/Aleem & Dixon Deliverables/Scraped Tweets"
+os.chdir(newwd)
+extension_name = 'csv'
+output_name = ""
+
+#Code
+
+all_filenames = [i for i in glob.glob('*.{}'.format(extension_name))]
+
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+combined_csv.to_csv( output_name, index=False, encoding='utf-8-sig')
